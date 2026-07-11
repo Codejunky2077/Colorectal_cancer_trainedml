29992df0d42053db43aa8fd107d2fcee35a4e907
Codejunky207 <aniketbhilai2042@gmail.com>
2026-07-12 00:57:42 +0530

    decision tree model ready

---
+++ decisiontreeml.py
@@ -0,0 +1,52 @@
+#using pandas to get info about datatype of file 
+import pandas as pd
+sample_file="CRC_ml/Gut_Microbiome_CRC_Dataset.csv"
+crc=pd.read_csv(sample_file)
+print(crc.info())
+
+#we start to labelencode the data of healthy and crc information for decision tree model
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+crc["Disease_status"]=le.fit_transform(crc["Disease_status"])
+
+#splitting the dataset into two 
+x=crc.drop(columns=["Sample_ID","Disease_status"])
+y=crc["Disease_status"]
+
+#using train_test_split to split the dataset into train and 20% of data to be used as test data
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(
+    x,y,test_size=0.2,random_state=40,stratify=y
+)
+
+#we are now checking the split of data in train and test data
+print(f"Train data shape in X {x_train.shape}")
+print(f"Test data shape in X {x_test.shape}")
+print(f"Train data shape in y {y_train.shape}")
+print(f"Test data shape in y {y_test.shape}")
+
+#we create empty decision tree model and fit the train data to it
+from sklearn.tree import DecisionTreeClassifier
+crc_model=DecisionTreeClassifier()
+
+#we fit the data 
+crc_model.fit(x_train,y_train)
+
+#now we will predict the test data using the model
+y_pred=crc_model.predict(x_test)
+print(y_pred[:10])
+print(y_test[:10])
+
+#now we analyse the prediction accuracy and confusion matrix to check the performance of the model
+from sklearn.metrics import accuracy_score,confusion_matrix
+accuracy=accuracy_score(y_test,y_pred)
+print(f"Accuracy score of model is {accuracy*100 :.2f}%")
+
+#confusion matrix to check the performance of the model
+confuse=confusion_matrix(y_test,y_pred)
+print("Confusion matrix is:")
+print(confuse)
+
+#classification report to check the performance of the model
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
---
+++ preprocessing.py
@@ -0,0 +1,43 @@
+#importing pandas and loading dataset
+import pandas as pd
+file="CRC_ml/Healthy and crc.xlsx"
+#creating categories
+healthy=pd.read_excel(file,sheet_name="CTR")
+crc=pd.read_excel(file,sheet_name="CRC")
+#checking dataset
+#print(healthy.head())
+#print(crc.head())
+
+#transposing dataset columns and rows to make it suitable for machine learning
+healthy = healthy.set_index(healthy.columns[0]).T
+crc = crc.set_index(crc.columns[0]).T
+#print(healthy.head())
+#print(crc.head())
+
+#adding new column to label healthy and crc samples
+healthy["Disease_status"]="Healthy"
+crc["Disease_status"]="CRC"
+
+#print(healthy.head())
+#print(crc.head())
+
+#creating a combined dataset suitable for ml models
+dataset=pd.concat([healthy, crc])
+print(dataset)
+
+#making sample id as new columns 
+dataset.reset_index(inplace=True)
+dataset.rename(columns={"index": "Sample_ID"}, inplace=True)
+#print(dataset)
+
+
+dataset = dataset.fillna(0)
+print("Remaining missing values:",
+      dataset.isna().sum().sum())
+print(dataset.head())
+#obtaing preprocessed dataset in csv format ready to be used for machine learning models
+dataset.to_csv(
+    r"CRC_ml/Gut_Microbiome_CRC_Dataset.csv",
+    index=False
+)
+print("Dataset successfully preprocessed and saved.")
